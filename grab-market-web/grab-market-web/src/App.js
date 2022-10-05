9a39ee8caf98a7bea29bdaca72b351ae11a35f6c
So Hyun Yang <91578131+ssonnni@users.noreply.github.com>
2022-10-05 12:30:24 +0900

    react 파일 연결

--- grab-market-web/grab-market-web/src/App.js
+++ grab-market-web/grab-market-web/src/App.js
@@ -1,21 +1,9 @@
-import { Route, Link, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
